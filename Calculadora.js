cf3adba9cbc95efcf7fac35ee850593babb97ec3
IgnacioBiglieri <nacho.biglieri00@gmail.com>
2022-07-07 22:54:20 -0300

    chiste monse

--- Calculadora.js
+++ Calculadora.js
@@ -18,7 +18,7 @@ class Calculadora {
 
     multiplicar(num1, num2) {
        if (num1 == 0 || num2 == 0) {
-        return '0'
+        return 'monse re puto'
        } else {
         return num1 * num2;
        }
